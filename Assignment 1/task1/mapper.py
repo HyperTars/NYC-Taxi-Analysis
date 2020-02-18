b8850b0b6e54fbf0143b9e8212999579a834a853
HyperTars <hypertars@gmail.com>
2020-02-18 09:36:24 -0500

    finish Task 1

--- Assignment 1/task1/mapper.py
+++ Assignment 1/task1/mapper.py
@@ -1,40 +1,39 @@
 #!/usr/bin/env python
-import os
-import string
 import sys
 
-cnt = 0
 for line in sys.stdin:
-    if len(line) <= 1:
+    # skip invalid data
+    line = line.strip()
+    if len(line) <= 1 or 'medallion' in line:
         continue
 
-    data = line.strip().split(',')
-    if data[0] == 'medallion':
-        continue
+    # split data
+    data = line.split(',')
 
     # fares
     if len(data) == 11:
-        key = data[0] + ',' + data[1] + ',' + data[2] + ',' + data[3]
-        val = data[4] + ',' + \
-            data[5] + ',' + \
-            data[6] + ',' + \
-            data[7] + ',' + \
-            data[8] + ',' + \
-            data[9] + ',' + \
-            data[10]
-        print '%s\t%s' % (key, val)
+        key = ','.join(data[0:4])
+        val = ','.join(data[4:])
+        print '%s\tf,%s' % (key, val)
 
     # trips
     if len(data) == 14:
-        key = data[0] + ',' + data[1] + ',' + data[2] + ',' + data[5]
-        val = data[3] + ',' + \
-            data[4] + ',' + \
-            data[6] + ',' + \
-            data[7] + ',' + \
-            data[8] + ',' + \
-            data[9] + ',' + \
-            data[10] + ',' + \
-            data[11] + ',' + \
-            data[12] + ',' + \
-            data[13]
-        print '%s\t%s' % (key, val)
+        key_ = ','.join(data[0:3])
+        key = key_ + ',' + data[5]
+        val = ','.join(data[3:5] + data[6:])
+        print '%s\tt,%s' % (key, val)
+
+# Test Code
+'''
+cd ~/hw1/Task1
+rm -rf TripFareJoinSamp.out
+hfs -rm -r TripFareJoinSamp.out
+hjs -D mapreduce.job.reduces=0 \
+-file ~/hw1/Task1/src/ \
+-mapper src/mapper.sh \
+-input /user/wl2154/fares_samp.csv /user/wl2154/trips_samp.csv \
+-output /user/wl2154/TripFareJoinSamp.out
+hfs -get TripFareJoinSamp.out
+hfs -getmerge TripFareJoinSamp.out TripFareJoinSamp.txt
+cat TripFareJoinSamp.txt
+'''
--- Assignment 1/task1/reducer.py
+++ Assignment 1/task1/reducer.py
@@ -1,36 +1,82 @@
 #!/usr/bin/env python
-from operator import itemgetter
 import sys
 
 prev_key = None
+key = None
 trips_val = []
 fares_val = []
-key = None
-val = None
+
+
+def output():
+    for trips in trips_val:
+        for fares in fares_val:
+            print '%s\t%s' % (prev_key, trips + ',' + fares)
+
+
 for line in sys.stdin:
-    line = line.strip()
-    key, val = line.split('\t', 1)
-    s_val = val.split(',')
+    # extract
+    key, val = line.strip().split('\t', 1)
+    tag = val.split(',')[0]
+    val = ','.join(val.split(',')[1:])
 
-    if key is None:
-        continue
+    # key exists, add pair
+    if key == prev_key and prev_key is not None:
+        if tag == 'f':
+            fares_val.append(val)
+        if tag == 't':
+            trips_val.append(val)
 
-    if key != prev_key:
-        # print
-        for trips in trips_val:
-            for fares in fares_val:
-                print '%s\t%s' % (prev_key, trips + ',' + fares)
+    # new key
+    else:
+        # print previous key pair
+        if (prev_key):
+            output()
+
         # update
         prev_key = key
         trips_val = []
         fares_val = []
+        if tag == 'f':
+            fares_val.append(val)
+        if tag == 't':
+            trips_val.append(val)
 
-    if len(s_val) == 7:
-        fares_val.append(val)
-    if len(s_val) == 10:
-        trips_val.append(val)
+# print last key pair
+if prev_key == key:
+    output()
 
+# Test Code
+'''
+cd ~/hw1/Task1
+rm -rf TripFareJoinSamp.out
+hfs -rm -r TripFareJoinSamp.out
+hjs -D mapreduce.job.reduces=1 \
+-file ~/hw1/Task1/src/ \
+-mapper src/mapper.sh \
+-reducer src/reducer.sh \
+-input /user/wl2154/fares_samp.csv /user/wl2154/trips_samp.csv \
+-output /user/wl2154/TripFareJoinSamp.out
+hfs -get TripFareJoinSamp.out
+hfs -getmerge TripFareJoinSamp.out TripFareJoinSamp.txt
+cat TripFareJoinSamp.txt
+'''
 
-for trips in trips_val:
-    for fares in fares_val:
-        print '%s\t%s' % (prev_key, trips + ',' + fares)
+# Run Code
+'''
+cd ~/hw1/Task1
+rm -rf TripFareJoin.out
+hfs -rm -r TripFareJoin.out
+hjs -D mapreduce.job.reduces=1 \
+-file ~/hw1/Task1/src/ \
+-mapper src/mapper.sh \
+-reducer src/reducer.sh \
+-input /user/wl2154/fare_data.csv /user/wl2154/trip_data.csv \
+-output /user/wl2154/TripFareJoin.out
+hfs -get TripFareJoin.out
+hfs -getmerge TripFareJoin.out TripFareJoin.txt
+hfs -rm TripFareJoin.txt
+hfs -put TripFareJoin.txt
+head TripFareJoin.txt
+tail TripFareJoin.txt
+wc -l TripFareJoin.txt
+'''
