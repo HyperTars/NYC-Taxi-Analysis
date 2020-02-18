9598e26ced6d8705b2da9c5e2da27fc4a592e575
HyperTars <hypertars@gmail.com>
2020-02-17 21:00:34 -0500

    git add .

--- Assignment 1/task1/mapper.py
+++ Assignment 1/task1/mapper.py
@@ -5,37 +5,36 @@ import sys
 
 cnt = 0
 for line in sys.stdin:
-        if len(line) <= 1:
-                contiue
+    if len(line) <= 1:
+        continue
 
-        data = line.strip().split(',')
+    data = line.strip().split(',')
+    if data[0] == 'medallion':
+        continue
 
-        if data[0] == 'medallion':
-                continue
+    # fares
+    if len(data) == 11:
+        key = data[0] + ',' + data[1] + ',' + data[2] + ',' + data[3]
+        val = data[4] + ',' + \
+            data[5] + ',' + \
+            data[6] + ',' + \
+            data[7] + ',' + \
+            data[8] + ',' + \
+            data[9] + ',' + \
+            data[10]
+        print '%s\t%s' % (key, val)
 
-        # fares
-        if len(data) == 11:
-                key = data[0] + ',' + data[1] + ',' + data[2] + ',' + data[3]
-                val = data[4] + ',' + \
-                        data[5] + ',' + \
-                        data[6] + ',' + \
-                        data[7] + ',' + \
-                        data[8] + ',' + \
-                        data[9] + ',' + \
-                        data[10]
-                print '%s\t%s' % (key, val)
-                
-        # trips
-        if len(data) == 14:
-                key = data[0] + ',' + data[1] + ',' + data[2] + ',' + data[5]
-                val = data[3] + ',' + \
-                        data[4] + ',' + \
-                        data[6] + ',' + \
-                        data[7] + ',' + \
-                        data[8] + ',' + \
-                        data[9] + ',' + \
-                        data[10] + ',' + \
-                        data[11] + ',' + \
-                        data[12] + ',' + \
-                        data[13]
-                print '%s\t%s' % (key, val)
+    # trips
+    if len(data) == 14:
+        key = data[0] + ',' + data[1] + ',' + data[2] + ',' + data[5]
+        val = data[3] + ',' + \
+            data[4] + ',' + \
+            data[6] + ',' + \
+            data[7] + ',' + \
+            data[8] + ',' + \
+            data[9] + ',' + \
+            data[10] + ',' + \
+            data[11] + ',' + \
+            data[12] + ',' + \
+            data[13]
+        print '%s\t%s' % (key, val)
