6b541aafc3479543c1956d7433c9ad1ff5972aa8
HyperTars <hypertars@gmail.com>
2020-02-18 10:56:57 -0500

    task3

--- Assignment 1/task3/mapper.py
+++ Assignment 1/task3/mapper.py
@@ -1,18 +1,33 @@
 #!/usr/bin/env python
-import os
-import string
 import sys
 import re
 for line in sys.stdin:
-    if len(line) <= 1:
+    # skip invalid
+    if len(line) <= 1 or 'medallion' in line:
         continue
+
+    # extract data
     data = line.strip()
     data = re.split(',|\t', data)
     key = data[0]
-    if key == 'medallion':
-        continue
     val = ','.join(data[1:])
-    if 'MEDALLION' in line or 'medallion' in line or 'CUR' in line or 'cur' in line:
+
+    if 'MEDALLION' in line or 'CUR' in line:
         print '%s\tlc,%s' % (key, val)
     else:
         print '%s\ttf,%s' % (key, val)
+
+# Test Code
+'''
+cd ~/hw1/Task3
+rm -rf VehicleJoinSamp.out
+hfs -rm -r VehicleJoinSamp.out
+hjs -D mapreduce.job.reduces=0 \
+-file ~/hw1/Task3/src/ \
+-mapper src/mapper.sh \
+-input /user/wl2154/TripFareJoinSamp.txt /user/wl2154/licenses_samp.csv \
+-output /user/wl2154/VehicleJoinSamp.out
+hfs -get VehicleJoinSamp.out
+hfs -getmerge VehicleJoinSamp.out VehicleJoinSamp.txt
+cat VehicleJoinSamp.txt
+'''
--- Assignment 1/task3/reducer.py
+++ Assignment 1/task3/reducer.py
@@ -1,35 +1,80 @@
 #!/usr/bin/env python
-from operator import itemgetter
 import sys
 
 prev_key = None
+key = None
 tf_val = []
 lc_val = []
-key = None
-val = None
+
+
+def output():
+    for tf in tf_val:
+        for lc in lc_val:
+            print '%s\t%s' % (prev_key, tf + ',' + lc)
+
+
 for line in sys.stdin:
-    line = line.strip()
-    key, val = line.split('\t', 1)
-    s_val = val.split(',')
+    # extract data
+    key, val = line.strip().split('\t', 1)
+    tag = val.split(',')[0]
+    val = ','.join(val.split(',')[1:])
 
-    if key is None:
-        continue
+    # key exists, add pair
+    if key == prev_key and prev_key is not None:
+        if tag == 'tf':
+            tf_val.append(val)
+        if tag == 'lc':
+            lc_val.append(val)
 
-    if key != prev_key:
-        # print
-        for tf in tf_val:
-            for lc in lc_val:
-                print '%s\t%s' % (prev_key, tf + ',' + lc)
+    # new key
+    else:
+        # print previous key pair
+        if (prev_key):
+            output()
+
         # update
         prev_key = key
         tf_val = []
         lc_val = []
+        if tag == 'tf':
+            tf_val.append(val)
+        if tag == 'lc':
+            lc_val.append(val)
 
-    if s_val[0] == 'tf':
-        tf_val.append(','.join(s_val[1:]))
-    if s_val[0] == 'lc':
-        lc_val.append(','.join(s_val[1:]))
-        
-for tf in tf_val:
-    for lc in lc_val:
-        print '%s\t%s' % (prev_key, tf + ',' + lc)
+# print last key pair
+if prev_key == key:
+    output()
+
+# Test Code
+'''
+cd ~/hw1/Task3
+rm -rf VehicleJoinSamp.out
+hfs -rm -r VehicleJoinSamp.out
+hjs -D mapreduce.job.reduces=1 \
+-file ~/hw1/Task3/src/ \
+-mapper src/mapper.sh \
+-reducer src/reducer.sh \
+-input /user/wl2154/TripFareJoinSamp.txt /user/wl2154/licenses_samp.csv \
+-output /user/wl2154/VehicleJoinSamp.out
+hfs -get VehicleJoinSamp.out
+hfs -getmerge VehicleJoinSamp.out VehicleJoinSamp.txt
+cat VehicleJoinSamp.txt
+'''
+
+# Run Code
+'''
+cd ~/hw1/Task3
+rm -rf VehicleJoin.out
+hfs -rm -r VehicleJoin.out
+hjs -D mapreduce.job.reduces=1 \
+-file ~/hw1/Task3/src/ \
+-mapper src/mapper.sh \
+-reducer src/reducer.sh \
+-input /user/wl2154/TripFareJoin.txt /user/wl2154/licenses.csv \
+-output /user/wl2154/VehicleJoin.out
+hfs -get VehicleJoin.out
+hfs -getmerge VehicleJoin.out VehicleJoin.txt
+head VehicleJoin.txt
+tail VehicleJoin.txt
+wc -l VehicleJoin.txt
+'''
