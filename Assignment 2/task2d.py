f6905abfbd532561cf92426e61c79bdf00c9ec40
HyperTars <hypertars@gmail.com>
2020-03-01 22:52:35 -0500

    task2d

---
+++ Assignment 2/task2d.py
@@ -0,0 +1,41 @@
+import sys
+from pyspark import SparkContext
+
+sc = SparkContext.getOrCreate()
+# file = sc.textFile("task1a.out")
+file = sc.textFile(sys.argv[1], 1)
+
+lines = file.map(lambda line: line.split(','))
+
+data = lines.map(lambda x: ((x[0]), 1))
+trips = data.countByKey()
+data = lines.map(lambda x: (x[0], x[3][:10]))
+days = data.distinct().countByKey()
+
+dic = {}
+for key in trips.keys():
+    val = []
+    val.append(trips[key])
+    if (days[key] != 0):
+        val.append('%.2f' % (trips[key] / days[key]))
+    else:
+        val.append('%.2f' % 0)
+    dic[key] = val
+
+output = sc.parallelize(list(dic.items()))
+output = output.map(lambda x: x[0] + ',' + str(x[1][0]) + ',' + str(x[1][1]))
+
+output.saveAsTextFile("task2d.out")
+
+sc.stop()
+
+'''
+module load python/gnu/3.4.4
+module load spark/2.2.0
+rm -rf task2d.out
+hfs -rm -R task2d.out
+spark-submit --conf \
+spark.pyspark.python=/share/apps/python/3.6.5/bin/python \
+task2d.py task1a.out
+hfs -getmerge task2d.out task2d.out
+'''
